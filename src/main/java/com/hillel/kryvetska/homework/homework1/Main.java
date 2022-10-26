4adc0084bdfe5e09019198ec66f0d74f138ce6d1
Kryvetska Anastasiia <nastya.kryvetska@gmail.com>
2022-10-26 21:57:19 +0300

    commit all files

---
+++ src/main/java/com/hillel/kryvetska/homework/homework1/Main.java
@@ -0,0 +1,9 @@
+package com.hillel.kryvetska.homework.homework1;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+
+    }
+}
---
+++ src/main/java/com/hillel/kryvetska/homework/homework5/Main.java
@@ -0,0 +1,26 @@
+package com.hillel.kryvetska.homework.homework5;public class Main {
+    public static void main(String[] args) {
+
+        int liWarrior = 13;
+        int liBowman = 24;
+        int liRider = 46;
+
+        int minWarrior = 9;
+        int minBowman = 35;
+        int minRider = 12;
+        int numberOfLi1Type = 860;
+
+        int simple = (int) (numberOfLi1Type * 1.5);
+
+        int numberOfMin1Type = simple + numberOfLi1Type;
+
+        int generalIndicatorOfLi = (numberOfLi1Type * liWarrior) + (numberOfLi1Type * liBowman) + (numberOfLi1Type * liRider);
+        int generalIndicatorOfMin = (numberOfMin1Type * minWarrior) + (numberOfMin1Type * minBowman) + (numberOfMin1Type + minRider);
+        int general = generalIndicatorOfLi + generalIndicatorOfMin;
+
+        System.out.println("Загальний показник атаки Лі = " + generalIndicatorOfLi);
+        System.out.println("Загальний показник атаки Мінь = " + generalIndicatorOfMin);
+        System.out.println("Загальний показник атаки обох династій = " + general);
+    }
+}
+
