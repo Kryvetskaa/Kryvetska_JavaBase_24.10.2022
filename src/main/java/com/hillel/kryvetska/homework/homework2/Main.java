32de917678387b072a110af24a2c8067a3d4ca15
Kryvetska Anastasiia <nastya.kryvetska@gmail.com>
2022-10-30 01:33:58 +0300

    commit all files

---
+++ src/main/java/com/hillel/kryvetska/homework/homework2/Main.java
@@ -0,0 +1,35 @@
+package com.hillel.kryvetska.homework.homework2;
+
+import java.sql.SQLOutput;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println();
+        System.out.println();
+
+        System.out.println("Brandenburg Gate");
+        String str = "latitude = " + "52.51627460°";
+        System.out.println(str);
+        String str2 = "longitude = " + "13.3755154°";
+        System.out.println(str2);
+
+        System.out.println();
+        System.out.println();
+
+        System.out.println("Brandenburg Gate");
+        System.out.println("latitude");
+        double latitude = 52.51627460;
+        System.out.println(latitude);
+        System.out.println("longitude");
+        double longitude = 13.3755154;
+        System.out.println(longitude);
+
+
+
+
+
+
+    }
+
+
+}
---
+++ src/main/java/com/hillel/kryvetska/homework/homework4/Main.java
@@ -0,0 +1,22 @@
+package com.hillel.kryvetska.homework.homework4;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+
+        int a = 11;
+        int b = 16;
+        int c = 10;
+
+        int volume = a * b * c;
+        System.out.println("Об'єм паралелепіпеда = " + volume + "см³");
+        int length = (11*4) + (16*4) + (10*4);
+        System.out.println("Сума всіх сторін = "+ length + "см");
+
+
+    }
+
+
+
+}
