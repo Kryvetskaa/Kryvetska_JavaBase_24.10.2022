8a78394445f51ac6c9ab12f6dbac8de1c028aa7c
Kryvetska Anastasiia <nastya.kryvetska@gmail.com>
2022-10-30 01:51:22 +0300

    commit all files

--- src/main/java/com/hillel/kryvetska/homework/homework5/Main.java
+++ src/main/java/com/hillel/kryvetska/homework/homework5/Main.java
@@ -1,22 +1,21 @@
-package com.hillel.kryvetska.homework.homework5;
-
-public class Main {
+package com.hillel.kryvetska.homework.homework5;public class Main {
     public static void main(String[] args) {
 
         int liWarrior = 13;
         int liBowman = 24;
         int liRider = 46;
+        int numberOfLi1Type = 860;
 
         int minWarrior = 9;
         int minBowman = 35;
         int minRider = 12;
-        int numberOfLi1Type = 860;
 
-        int numberOfMin1Type = (int) (numberOfLi1Type * 1.5);
+        double simple = 1.5;
 
+        int numberOfMin1Type = (int) (numberOfLi1Type * simple);
 
-        int generalIndicatorOfLi = (numberOfLi1Type * liWarrior)+(numberOfLi1Type * liBowman)+(numberOfLi1Type * liRider);
-        int generalIndicatorOfMin = (numberOfMin1Type * minWarrior)+(numberOfMin1Type * minBowman)+(numberOfMin1Type * minRider);
+        int generalIndicatorOfLi = (numberOfLi1Type * liWarrior) + (numberOfLi1Type * liBowman) + (numberOfLi1Type * liRider);
+        int generalIndicatorOfMin = (numberOfMin1Type * minWarrior) + (numberOfMin1Type * minBowman) + (numberOfMin1Type * minRider);
         int general = generalIndicatorOfLi + generalIndicatorOfMin;
 
         System.out.println("Загальний показник атаки Лі = " + generalIndicatorOfLi);
