dc2f0cdc93f3b8dc1ae657269d16207d8b6ec945
Kryvetska Anastasiia <nastya.kryvetska@gmail.com>
2022-11-09 21:42:13 +0200

    commit all files

--- src/main/java/com/hillel/kryvetska/homework/homework7/Main.java
+++ src/main/java/com/hillel/kryvetska/homework/homework7/Main.java
@@ -7,102 +7,44 @@ public class Main {
 
         Scanner scanner = new Scanner(System.in);
 
-        int a = (int) (Math.random() + 11);
+        int a = (int) ( Math.random() * 11 );
 
-        System.out.println("Guess the number from 0 to 10");
+        int i = 2;
 
-        while (true) {
+        while ( i > -1) {
             if (scanner.hasNextInt()) {
                 System.out.println(" ");
-                break;
-            }  else {
-                System.out.println("Wrong data, try again!");
-                scanner.nextLine();
-            }
-        }
-        int number1  = scanner.nextInt();
-        scanner.nextLine();
-        if ( number1 == a) {
-                System.out.println("Congratulations!!!!!");
+                int number = scanner.nextInt();
+                if (number == a) {
+                    System.out.println("Congratulations!!!!!!");
+                    break;
+                } else {
+                    System.out.println("you entered:" + " " + number + "    " + i + " " + "tries left");
+                    scanner.nextLine();
+                } if (i == 0 && number != a) {
+                    System.out.println("Game over... You lose");
+                }
             } else {
-            System.out.println("You entered number: " + number1 + "    " + "2 tries left");
-            scanner.nextLine();
-            }
-
-        while (true) {
-            if (scanner.hasNextInt()) {
-                System.out.println(" ");
-                break;
-            }  else {
-                System.out.println("Wrong data, try again!");
+                System.out.println("Wrong data, try again");
                 scanner.nextLine();
+                continue;
             }
-        }
-
-        int number2  = scanner.nextInt();
-        scanner.nextLine();
-        if ( number2 == a) {
-            System.out.println("Congratulations!!!!!");
-            scanner.nextLine();
-        } else {
-            System.out.println("You entered number: " + number1 + "    " + "1 trie left");
-            scanner.nextLine();
-        }
-
-        while (true) {
-            if (scanner.hasNextInt()) {
-                System.out.println(" ");
-                break;
-            }  else {
-                System.out.println("Wrong data, try again!");
-                scanner.nextLine();
-            }
-        }
-        int number3  = scanner.nextInt();
-        scanner.nextLine();
-        if ( number3 == a) {
-            System.out.println("Congratulations!!!!!");
-        } else {
-            System.out.println("You entered number: " + number1 + "    " + "0 tries left");
-            scanner.nextLine();
+            i--;
         }
 
 
 
-//        int number1  = scanner.nextInt();
-//        scanner.nextLine();
-//            if (number1 == a) {
-//                System.out.println("Congratulations!!!!!");
-//                break;
-//            } else if (number1 != a){
-//                System.out.println("You entered number: " + number1 + "    " + "2 tries left");
-//            } else {
-//                System.out.println("Wrong data, try again!");
-//                scanner.nextLine();
-//            }
 
-//
-//        while (true) {
-//            if (scanner.hasNextInt() && number1 == a) {
-//                System.out.println("Congratulations!!!!!");
-//                break;
-//            } else if (number1 != a){
-//                System.out.println("You entered number: " + number1 + "    " + "2 tries left");
-//            } else {
-//                System.out.println("Wrong data, try again!");
-//                scanner.nextLine();
-//            }
-//        }
-//        for (;;) {
-//            if (scanner.hasNextInt()) {
-//                int number = scanner.nextInt();
-//                System.out.println("You entered number: " + number);
-//                break;
-//            } else {
-//                System.out.println("Wrong data, try again!");
-//                scanner.nextLine();
-//            }
+
+
+
+
+
+
+
+
+
+
         }
     }
-
 
