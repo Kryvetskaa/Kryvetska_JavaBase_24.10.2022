79b360d9c8872c1022128dec933e0e7a4dfa944c
Kryvetska Anastasiia <nastya.kryvetska@gmail.com>
2022-12-07 21:45:54 +0200

    commit all files

---
+++ src/main/java/com/hillel/kryvetska/homework/homework14/Information.java
@@ -0,0 +1,273 @@
+package com.hillel.kryvetska.homework.homework14;
+
+import java.util.Scanner;
+
+public class Information {
+        Scanner scanner = new Scanner(System.in);
+        private String firstName = firstName(scanner);
+        public String lastName = getLine2(scanner);
+        private int day = day(scanner);
+        private int month = month(scanner);
+        private int year = year(scanner);
+        private String email = email(scanner);
+        private int number = number(scanner);
+        public int weight = getNumber5(scanner);
+        public int pressure = getNumber6(scanner);
+        public int steps = getNumber7(scanner);
+
+        public static void printAccountInfo() {
+                Information Information = new Information();
+                System.out.println(Information);
+        }
+
+        public Information () {
+                this.firstName = getFirstName();
+                this.lastName = lastName;
+                this.day = getDay();
+                this.month = getMonth();
+                this.year = getYear();
+                this.email = getEmail();
+                this.number = getNumber();
+                this.weight = weight;
+                this.pressure = pressure;
+                this.steps = steps;
+        }
+
+        public Information (String firstName, String lastName,
+                            int day, int month, int year,
+                            String email, int number, int weight,
+                            int pressure, int steps ) {
+                this.firstName = firstName(scanner);
+                this.lastName = getLine2(scanner);
+                this.day = day(scanner);
+                this.month = month(scanner);
+                this.year = year(scanner);
+                this.email = email(scanner);
+                this.number = number(scanner);
+                this.weight = getNumber5(scanner);
+                this.pressure = getNumber6(scanner);
+                this.steps = getNumber7(scanner);
+
+        }
+
+        public String getFirstName(){
+                return this.firstName;
+        }
+        public void setFirstName (String name){
+                this.firstName = firstName;
+        }
+        static String firstName (Scanner scanner) {
+                String firstName;
+                System.out.println("Please enter your first name");
+                while (true) {
+                        if (scanner.hasNextLine()) {
+                                firstName = scanner.nextLine();
+                                return firstName ;
+                        } else {
+                                System.out.println("Wrong data, try again");
+                                scanner.nextLine();
+                        }
+                }
+        }
+
+
+        static String getLine2 (Scanner scanner) {
+                String lastName;
+                System.out.println("Please enter your last name");
+                while (true) {
+                        if (scanner.hasNextLine()) {
+                                lastName = scanner.nextLine();
+                                return lastName ;
+                        } else {
+                                System.out.println("Wrong data, try again");
+                                scanner.nextLine();
+                        }
+                }
+        }
+
+        public int getDay(){
+                return this.day;
+        }
+        public void setDay (){
+                this.day = day;
+        }
+        static int day (Scanner scanner) {
+                int day = 0;
+                System.out.println("Please enter your birth day");
+                while (true) {
+                        if (scanner.hasNextInt()) {
+                                day = scanner.nextInt();
+                                if (day < 0) {
+                                        System.out.println("Please enter positive number");
+                                } else {
+                                        break;
+                                }
+                                scanner.nextLine();
+                        } else {
+                                System.out.println("Wrong data, try again");
+                                scanner.nextLine();
+                        }
+                }
+                return day;
+        }
+
+        public int getMonth(){
+                return this.month;
+        }
+        public void setMonth (){
+                this.month = month;
+        }
+        static int month (Scanner scanner) {
+                int month = 0;
+                System.out.println("Please enter your birth month");
+                while (true) {
+                        if (scanner.hasNextInt()) {
+                                month = scanner.nextInt();
+                                if (month < 0) {
+                                        System.out.println("Please enter positive number");
+                                } else {
+                                        break;
+                                }
+                                scanner.nextLine();
+                        } else {
+                                System.out.println("Wrong data, try again");
+                                scanner.nextLine();
+                        }
+                }
+                return month;
+        }
+
+        public int getYear(){
+                return this.year;
+        }
+        public void setYear (){
+                this.year = year;
+        }
+        static int year (Scanner scanner) {
+                int year = 0;
+                System.out.println("Please enter your birth year");
+                while (true) {
+                        if (scanner.hasNextInt()) {
+                                year = scanner.nextInt();
+                                if (year < 0) {
+                                        System.out.println("Please enter positive number");
+                                } else {
+                                        break;
+                                }
+                                scanner.nextLine();
+                        } else {
+                                System.out.println("Wrong data, try again");
+                                scanner.nextLine();
+                        }
+                }
+                return year;
+        }
+
+        public String getEmail(){
+                return this.email;
+        }
+        public void setEmail (String name){
+                this.email = email;
+        }
+        static String email (Scanner scanner) {
+                String email;
+                System.out.println("Please enter your email");
+                while (true) {
+                        if (scanner.hasNextLine()) {
+                                email = scanner.nextLine();
+                                return email;
+                        } else {
+                                System.out.println("Wrong data, try again");
+                                scanner.nextLine();
+                        }
+                }
+        }
+
+        public int getNumber(){
+                return this.number;
+        }
+        public void setNumber (){
+                this.number = number;
+        }
+        static int number (Scanner scanner) {
+                int number = 0;
+                System.out.println("Please enter your phone number");
+                while (true) {
+                        if (scanner.hasNextInt()) {
+                                number = scanner.nextInt();
+                                if (number < 0) {
+                                        System.out.println("Please enter positive number");
+                                } else {
+                                        break;
+                                }
+                                scanner.nextLine();
+                        } else {
+                                System.out.println("Wrong data, try again");
+                                scanner.nextLine();
+                        }
+                }
+                return number;
+        }
+
+        static int getNumber5(Scanner scanner) {
+                int weight = 0;
+                System.out.println("Please enter your weight");
+                while (true) {
+                        if (scanner.hasNextInt()) {
+                                weight = scanner.nextInt();
+                                if (weight < 0) {
+                                        System.out.println("Please enter positive number");
+                                } else {
+                                        break;
+                                }
+                                scanner.nextLine();
+                        } else {
+                                System.out.println("Wrong data, try again");
+                                scanner.nextLine();
+                        }
+                }
+                return weight;
+        }
+
+        static int getNumber6(Scanner scanner) {
+                int pressure = 0;
+                System.out.println("Please enter your blood pressure");
+                while (true) {
+                        if (scanner.hasNextInt()) {
+                                pressure = scanner.nextInt();
+                                if (pressure < 0) {
+                                        System.out.println("Please enter positive number");
+                                } else {
+                                        break;
+                                }
+                                scanner.nextLine();
+                        } else {
+                                System.out.println("Wrong data, try again");
+                                scanner.nextLine();
+                        }
+                }
+                return pressure;
+        }
+
+        static int getNumber7(Scanner scanner) {
+                int steps = 0;
+                System.out.println("Please enter the number of steps");
+                while (true) {
+                        if (scanner.hasNextInt()) {
+                                steps = scanner.nextInt();
+                                if (steps < 0) {
+                                        System.out.println("Please enter positive number");
+                                } else {
+                                        break;
+                                }
+                                scanner.nextLine();
+                        } else {
+                                System.out.println("Wrong data, try again");
+                                scanner.nextLine();
+                        }
+                }
+                return steps;
+        }
+
+
+}
---
+++ src/main/java/com/hillel/kryvetska/homework/homework14/Main.java
@@ -0,0 +1,18 @@
+package com.hillel.kryvetska.homework.homework14;
+
+public class Main extends Information {
+    public static void main(String[] args) {
+
+        printAccountInfo();
+
+
+
+
+
+
+
+
+
+
+    }
+}
