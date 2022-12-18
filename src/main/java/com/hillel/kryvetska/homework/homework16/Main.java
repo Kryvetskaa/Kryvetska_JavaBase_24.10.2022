d70b2f0fcd73eaefff0826baa06828c08dbec689
Kryvetska Anastasiia <nastya.kryvetska@gmail.com>
2022-12-18 17:26:36 +0200

    commit all files

--- src/main/java/com/hillel/kryvetska/homework/homework16/Main.java
+++ src/main/java/com/hillel/kryvetska/homework/homework16/Main.java
@@ -4,7 +4,7 @@ public class Main {
     public static void main(String[] args) {
 
         MusicStyles[] musicStyles = {
-                new PopMusic("Pops Band"),
+                new PopMusic("Pop Band"),
                 new RockMusic("Rock Band"),
                 new ClassicMusic("Classic Band")
         };
